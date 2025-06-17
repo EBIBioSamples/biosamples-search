c664f55e05fffbffdd2c937b91ee97493c000218
Isuru Liyanage <theisuru@gmail.com>
2025-06-17 10:52:32 +0100

    restructure project

--- src/main/java/uk/ac/ebi/biosamples/search/samples/SamplesRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.biosamples.search.samples;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SamplesRepository extends ElasticsearchRepository<Sample, String> {
-
-}
