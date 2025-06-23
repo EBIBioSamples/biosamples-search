452748f55b35c16e755e3bc9eb8c5a21d69d9de8
Isuru Liyanage <theisuru@gmail.com>
2025-06-23 10:23:28 +0100

    rabbit connection

--- server/src/main/java/uk/ac/ebi/biosamples/search/BiosamplesSearchApplication.java
+++ server/src/main/java/uk/ac/ebi/biosamples/search/BiosamplesSearchApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 
-@SpringBootApplication(exclude = RabbitAutoConfiguration.class)
+//@SpringBootApplication(exclude = RabbitAutoConfiguration.class)
+@SpringBootApplication
 public class BiosamplesSearchApplication {
 
   public static void main(String[] args) {
--- server/src/main/java/uk/ac/ebi/biosamples/search/index/RabbitConfig.java
+++ server/src/main/java/uk/ac/ebi/biosamples/search/index/RabbitConfig.java
@@ -10,7 +10,7 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class RabbitConfig {
   public static final String INDEXING_QUEUE = "biosamples.tobeindexed.solr";
   public static final String INDEXING_EXCHANGE = "biosamples.forindexing.solr";
