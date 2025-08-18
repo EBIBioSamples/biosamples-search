be8e33023514cdb73dfa995d0534189b552993ab
Isuru Liyanage <theisuru@gmail.com>
2025-08-18 13:15:11 +0100

    bug: remove surpressed from search

--- server/src/main/java/uk/ac/ebi/biosamples/search/samples/filter/PublicSearchFilter.java
+++ server/src/main/java/uk/ac/ebi/biosamples/search/samples/filter/PublicSearchFilter.java
@@ -14,11 +14,21 @@ public record PublicSearchFilter(String webinId) implements SearchFilter {
   public Query getQuery() {
     Query publicDateQuery = new DateRangeSearchFilter(
         DateRangeSearchFilter.DateField.RELEASE, null, Instant.now().toString()).getQuery();
+
+    Query suppressedStatusQuery =
+        new AttributeSearchFilter("INSDC status", List.of("suppressed")).getQuery();
+
+    Query publicQuery = new BoolQuery.Builder()
+        .must(List.of(publicDateQuery))
+        .mustNot(List.of(suppressedStatusQuery))
+        .build()
+        ._toQuery();
+
     if (StringUtils.hasText(webinId)) {
       Query authQuery = new WebinIdSearchFilter(webinId).getQuery();
-      return new BoolQuery.Builder().should(List.of(authQuery, publicDateQuery)).build()._toQuery();
+      return new BoolQuery.Builder().should(List.of(authQuery, publicQuery)).build()._toQuery();
     }
 
-    return publicDateQuery;
+    return publicQuery;
   }
 }
