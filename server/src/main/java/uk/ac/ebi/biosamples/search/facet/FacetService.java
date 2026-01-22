55f244e577b2c3c871af94ebf0af508e90c383c4
dgupta <dgupta@ebi.ac.uk>
2026-01-22 13:55:25 +0000

    Getting ES and faceting working

--- server/src/main/java/uk/ac/ebi/biosamples/search/facet/FacetService.java
+++ server/src/main/java/uk/ac/ebi/biosamples/search/facet/FacetService.java
@@ -34,8 +34,9 @@ public class FacetService {
   public List<Facet> getFacets(SearchQuery searchQuery) {
     Query esSearchQuery = QueryHelper.getSearchQuery(searchQuery);
     Map<String, Aggregation> aggregations = getAggregations(searchQuery);
+    log.info("Generated Elasticsearch Aggregations: {} aggregations configured", aggregations.size());
     NativeQuery query = getEsNativeQuery(esSearchQuery, aggregations);
-    return retrieveFacets(query);
+    return retrieveFacets(query, aggregations);
   }
 
   private Map<String, Aggregation> getAggregations(SearchQuery searchQuery) {
@@ -60,8 +61,9 @@ public class FacetService {
     return builder.build();
   }
 
-  public List<Facet> retrieveFacets(NativeQuery query) {
+  public List<Facet> retrieveFacets(NativeQuery query, Map<String, Aggregation> aggregations) {
     log.info("Generated Elasticsearch Query: {}", query.getQuery());
+    log.info("Generated Elasticsearch Aggregations ({}): {}", aggregations.size(), aggregations.keySet());
     SearchHits<Sample> hits = elasticsearchOperations.search(query, Sample.class);
     return facetingStrategy.retrieveFacets(hits);
   }
